aa851aa8027e6a48a487692ed7a5a22ed00af570
Wouter van Reeven <wvanreeven@lsst.org>
2020-04-14 10:49:39 -0400

    Renamed a few variables. Added the reference paper for the alignment error computation.

--- python/reeven/van/astro/math/alignment_error_util.py
+++ python/reeven/van/astro/math/alignment_error_util.py
@@ -1,11 +1,13 @@
-from astropy import units as u
 from astropy.coordinates import SkyCoord, Angle, Latitude
 import math
 
 
-def compute_alignment_error(lat, s1, s2, err_ra, err_de):
-    """Compute the alignment error for the given latitdue given the measured sky coordinates and errors in
-    right ascentsion and declination.
+def compute_alignment_error(lat, s1, s2, err_ra, err_dec):
+    """Compute the alignment error for the given latitdue, the given measured sky coordinates and the given
+    errors in right ascentsion and declination.
+
+    This computation is based on the Two Star Polar Alignment paper by Ralph Pass in the misc/docs
+    directory of the source tree on GitHub.
 
     Parameters
     ----------
@@ -17,22 +19,25 @@ def compute_alignment_error(lat, s1, s2, err_ra, err_de):
         The hour angle and declination of the second alignment star.
     err_ra: `Angle`
         The error in right ascension as measured for the second star.
-    err_de: `Angle`
+    err_dec: `Angle`
         The error in declination as measured for the second star.
 
     Returns
     -------
-    The computed alignment errors given in offsets in elevation and azimuth.
+    delta_e, delta_a: `tuple` of `Angle`
+        The computed alignment errors given in offsets in elevation and azimuth.
 
     """
+    # Compute the determinant
     d = (
         math.cos(lat.radian)
         * (math.tan(s1.dec.radian) + math.tan(s2.dec.radian))
         * (1 - math.cos(s1.ra.radian - s2.ra.radian))
     )
 
-    one = math.cos(lat.radian) * (math.sin(s2.ra.radian) - math.sin(s1.ra.radian)) / d
-    two = (
+    # Compute the four matrix elements using the determinant
+    a11 = math.cos(lat.radian) * (math.sin(s2.ra.radian) - math.sin(s1.ra.radian)) / d
+    a12 = (
         -math.cos(lat.radian)
         * (
             math.tan(s1.dec.radian) * math.cos(s1.ra.radian)
@@ -40,11 +45,13 @@ def compute_alignment_error(lat, s1, s2, err_ra, err_de):
         )
         / d
     )
-    three = (math.cos(s1.ra.radian) - math.cos(s2.ra.radian)) / d
-    four = (
+    a21 = (math.cos(s1.ra.radian) - math.cos(s2.ra.radian)) / d
+    a22 = (
         math.tan(s2.dec.radian) * math.sin(s2.ra.radian)
         - math.tan(s1.dec.radian) * math.sin(s1.ra.radian)
     ) / d
-    delta_e = one * err_ra + two * err_de
-    delta_a = three * err_ra + four * err_de
+
+    # Compute the errors in elevaqtion and azimuth using the four matrix elements
+    delta_e = a11 * err_ra + a12 * err_dec
+    delta_a = a21 * err_ra + a22 * err_dec
     return delta_e, delta_a
--- tests/reeven/van/astro/math/test_alignment_error_util.py
+++ tests/reeven/van/astro/math/test_alignment_error_util.py
@@ -10,9 +10,9 @@ class Test(TestCase):
         s1 = SkyCoord(ra=3 * u.hourangle, dec=48 * u.degree)
         s2 = SkyCoord(ra=23 * u.hourangle, dec=45 * u.degree)
         err_ra = Angle(-12 * u.arcmin)
-        err_de = Angle(-21 * u.arcmin)
+        err_dec = Angle(-21 * u.arcmin)
         delta_e, delta_a = alignment_error_util.compute_alignment_error(
-            lat, s1, s2, err_ra, err_de
+            lat, s1, s2, err_ra, err_dec
         )
         self.assertAlmostEqual(delta_e.radian, Angle(7.3897 * u.arcmin).radian)
         self.assertAlmostEqual(delta_a.radian, Angle(32.2597 * u.arcmin).radian)
