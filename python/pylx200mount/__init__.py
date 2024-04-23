ba5c0f0010523f20958839a31e34cbbdc5a5b262
Wouter van Reeven <wouter@van.reeven.nl>
2024-04-23 22:02:24 +0200

    Add ASI camera implementation and AsiImageTaker utility class.

--- python/pylx200mount/__init__.py
+++ python/pylx200mount/__init__.py
@@ -1,4 +1,4 @@
-from . import alignment, camera, controller, emulation, motor, my_math, phidgets
+from . import alignment, asi, camera, controller, emulation, motor, my_math, phidgets
 from .datetime_util import *
 from .enums import *
 from .lx200_mount import *
