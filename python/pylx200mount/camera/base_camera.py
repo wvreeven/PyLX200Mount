36acbcc01508b1a879dcf7d5431ebdc47b35ca1b
Wouter van Reeven <wouter@van.reeven.nl>
2024-07-21 20:17:35 +0200

    Improve the handling of camera load errors.

--- python/pylx200mount/camera/base_camera.py
+++ python/pylx200mount/camera/base_camera.py
@@ -6,6 +6,8 @@ import numpy as np
 
 
 class BaseCamera(abc.ABC):
+    """Interface for all camera implementations."""
+
     def __init__(self) -> None:
         self.camera_id = 0
         self.img_width = 0
@@ -13,20 +15,46 @@ class BaseCamera(abc.ABC):
 
     @abc.abstractmethod
     async def open(self) -> None:
+        """Open the camera."""
         raise NotImplementedError
 
     @abc.abstractmethod
     async def set_max_image_size(self) -> None:
+        """Set the maximum image size.
+
+        Also set the bit depth to the required value.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     async def set_gain(self, gain: int) -> None:
+        """Set the gain.
+
+        Parameters
+        ----------
+        gain : `int`
+            The gain to set.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     async def set_exposure_time(self, exposure_time: int) -> None:
+        """Set the exposure time.
+
+        Parameters
+        ----------
+        exposure_time : `int`
+            The exposure time [microseconds] to set.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     async def take_and_get_image(self) -> np.ndarray:
+        """Take and image and return it.
+
+        Returns
+        -------
+        numpy.ndarray
+            The image as a numpy array.
+        """
         raise NotImplementedError
